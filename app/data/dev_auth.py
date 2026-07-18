67ac9149a6e6366ba4a1a6662f20cc72f8f5908d
The Librarian <schmid.nico@proton.me>
2026-07-18 13:25:05 +0200

    forgot my password + i should get more sleep before writing code

--- app/data/dev_auth.py
+++ app/data/dev_auth.py
@@ -1,3 +1,4 @@
 # if you really wanna put effort into figuring out the password based on the hash, have fun.
 # all its gonna give you is a simulator tool used for debugging :3
-DEV_HASH = "pbkdf2:sha256:260000:fda485662e9be4b5f5d275b08b2e8dbd:c371e1414303d91e8a7fa03eb8aa9378c936abe785142dc61c4d26d58a133df7"
+# it is now three weeks later and im amazed about how much of an idiot i can be when i had too little sleep and too much coffee. If you know you know.
+DEV_HASH = "pbkdf2:sha256:260000:6eff045330047de6c0e0b4242898a3ef:27e7f684561322d87d4ddb64e9b8924fd5f6825c0c685ca1fd05f6ac9fe6a757"
